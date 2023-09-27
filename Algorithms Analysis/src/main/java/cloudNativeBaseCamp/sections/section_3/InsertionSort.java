db1bb51dd5f53fcd0afc56ec97e6536a7b8e5518
Khaled Taha <khaledtaha105@gmail.com>
2023-09-27 13:07:24 +0300

    Selection Sort

--- Algorithms Analysis/src/main/java/cloudNativeBaseCamp/sections/section_3/InsertionSort.java
+++ Algorithms Analysis/src/main/java/cloudNativeBaseCamp/sections/section_3/InsertionSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args) {
         Number[] arr = {9, 5.5, 5, -1, 1, 0, 10};
-        insertionSort(arr);
+        selectionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -23,6 +23,25 @@ public class InsertionSort {
             arr[j + 1] = temp; // 1
         }
         // T(n) = n + n2 + 5 => O(n^2)
+    }
+
+    public static <T extends Number> void selectionSort(T[] arr){
+        int minIndex;
+        T temp;
+
+        for (int i = 0; i < arr.length - 1; i++) { // n
+            minIndex = i; // 1
+            for (int j = i + 1; j < arr.length; j++) { // n * n
+                if(arr[j].doubleValue() < arr[minIndex].doubleValue()){ // 1
+                    minIndex = j; // 1
+                }
+            }
+            temp = arr[i]; // 1
+            arr[i] = arr[minIndex]; // 1
+            arr[minIndex] = temp; // 1
+        }
+        // T(n) = n + n2 + 6 => O(n^2)
+        // In-place sort (requires no additional storage space)
     }
 }
 
