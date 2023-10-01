7cf54e730d0fdead1381178e0a7c55cd9f30817e
Khaled Taha <khaledtaha105@gmail.com>
2023-10-01 10:36:30 +0300

    Binary Search

---
+++ Algorithms Analysis/src/main/java/cloudNativeBaseCamp/sections/section_5/BinarySearch.java
@@ -0,0 +1,61 @@
+package cloudNativeBaseCamp.sections.section_5;
+
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Student[] students = {
+                new Student(100, "Alice"),
+                new Student(101, "Bob"),
+                new Student(102, "Charlie"),
+                new Student(103, "David"),
+                new Student(104, "Eve")
+        };
+        boolean found = binarySearch(students, new Student(102, "Charlie"));
+        System.out.println(found);
+    }
+
+    /**
+     * T(n) = O(log(n))
+     * S(n) = O(1) ==>  It uses constant space regardless of the size of the input data.
+     * @param arr
+     * @param element
+     * @return true or false
+     * @param <T>
+     */
+    public static <T extends Comparable<T>> boolean binarySearch(T[] arr, T element) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+            int compare = arr[mid].compareTo(element);
+
+            if (compare == 0) return true;
+            if (compare < 0) start = mid + 1;
+            else end = mid - 1;
+
+        }
+        return false;
+    }
+}
+
+class Student implements Comparable<Student>{
+    Integer id;
+    String name;
+
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        int idComparison = id.compareTo(student.id);
+        if (idComparison != 0) {
+            return idComparison;
+        }
+        return Character.compare(name.charAt(0), student.name.charAt(0));
+    }
+}
+
