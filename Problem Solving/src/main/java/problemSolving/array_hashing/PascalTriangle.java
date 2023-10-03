9f0c1340856a22d46898e4abab9a6c6fb11ce4c4
Khaled Taha <khaledtaha105@gmail.com>
2023-10-03 10:07:06 +0300

    Pascal's Triangle

---
+++ Problem Solving/src/main/java/problemSolving/array_hashing/PascalTriangle.java
@@ -0,0 +1,57 @@
+package problemSolving.array_hashing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an integer numRows, return the first numRows of Pascal's triangle.
+ *
+ * In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+ *
+ * Example 1:
+ *
+ * Input: numRows = 5
+ * Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ * Example 2:
+ *
+ * Input: numRows = 1
+ * Output: [[1]]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= numRows <= 30
+ */
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        System.out.println(solution(1));
+        System.out.println(solution(2));
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+        System.out.println(solution(5));
+
+    }
+
+    public static List<List<Integer>> solution(int numRows) { // O(n^2) ==> O(30^2) = 900
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    int prev1 = triangle.get(i - 1).get(j - 1);
+                    int prev2 = triangle.get(i - 1).get(j);
+                    row.add(prev1 + prev2);
+                }
+            }
+
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+}
