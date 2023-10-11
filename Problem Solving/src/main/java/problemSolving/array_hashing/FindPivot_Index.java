0a01ca9c4118ace28ad3d801705b63defadeb948
Khaled Taha <khaledtaha105@gmail.com>
2023-10-11 21:22:20 +0300

    public int pivotIndex(int[] nums) { problem

---
+++ Problem Solving/src/main/java/problemSolving/array_hashing/FindPivot_Index.java
@@ -0,0 +1,19 @@
+package problemSolving.array_hashing;
+
+public class FindPivot_Index {
+
+    public int pivotIndex(int[] nums) {
+        int left, right, total = 0;
+        for (int num : nums) total += num;
+
+        for (int i = 0; i < nums.length; i++) {
+            left = (i == 0) ? 0 : nums[i - 1];
+            right = (i == nums.length - 1) ? 0 : total - (left + nums[i]);
+
+            if(left == right) return i;
+            else nums[i] += left;
+        }
+        return -1;
+    }
+
+}
