6a5c5f46a77680a00ecc37f9683d20c5a646635a
khaled-taha-2023 <khaled.elattar@kartngo.co>
2024-02-26 07:52:18 +0200

    Add notes for Java Collections During practice

---
+++ Problem Solving/src/main/java/problemSolving/array_hashing/Main.java
@@ -0,0 +1,11 @@
+package problemSolving.array_hashing;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ Problem Solving/src/main/resources/JAVA_DS.md
@@ -0,0 +1,11 @@
+# Java Collections notes
+
+## HashSet
+
+### Put nums in HashSet
+
+```java
+int[] nums = new int[]{1,2,3,4};
+HashSet<Integer> integers = new HashSet<>();
+for(int num : nums) integers.add(num);
+```
