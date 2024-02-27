5655ab96fbbb77bea651cb2585b50dd0fb71f979
khaled-taha-2023 <khaled.elattar@kartngo.co>
2024-02-27 15:29:30 +0200

    Add solution 2 for ReplaceWithGreatest problem

--- Problem Solving/src/main/java/problemSolving/array_hashing/ReplaceWithGreatest.java
+++ Problem Solving/src/main/java/problemSolving/array_hashing/ReplaceWithGreatest.java
@@ -48,4 +48,17 @@ public class ReplaceWithGreatest {
         }
         return arr;
     }
+
+    public int[] replaceElements_V2(int[] arr) {
+        int max, temp;
+        max = arr[arr.length - 1];
+        arr[arr.length - 1] = -1;
+
+        for (int i = arr.length - 2; i >= 0 ; i--) {
+            temp = arr[i];
+            arr[i] = max;
+            if(temp > max) max = temp;
+        }
+        return arr;
+    }
 }
