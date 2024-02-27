3f540deab5fddc3ff13a668962acba1a17ec364d
khaled-taha-2023 <khaled.elattar@kartngo.co>
2024-02-27 08:25:27 +0200

    Add solution3 to IsAnagram problem

--- Problem Solving/src/main/java/problemSolving/array_hashing/Main.java
+++ Problem Solving/src/main/java/problemSolving/array_hashing/Main.java
@@ -1,11 +1,11 @@
 package problemSolving.array_hashing;
 
-import java.util.Arrays;
-import java.util.Collections;
+
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
     }
 }
--- Problem Solving/src/main/java/problemSolving/array_hashing/ValidAnagram.java
+++ Problem Solving/src/main/java/problemSolving/array_hashing/ValidAnagram.java
@@ -60,4 +60,16 @@ public class ValidAnagram {
         return true;
     }
 
+    public static boolean solution3(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        int[] arr = new int[26];
+        for(char ch : s.toCharArray()) arr[ch - 'a']++;
+        for(char ch : t.toCharArray()){
+            if(arr[ch - 'a'] == 0) return false;
+            else arr[ch - 'a']--;
+        }
+        return true;
+    }
+
 }
